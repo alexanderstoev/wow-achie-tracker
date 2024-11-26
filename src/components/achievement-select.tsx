2a821f3babf46fe74bbab697d22c0bbd7b3722c3
Alex Stoev <alex.stoev@gmail.com>
2024-11-26 08:23:11 +0200

    Cleanup of default code

---
+++ src/components/achievement-select.tsx
@@ -0,0 +1,3 @@
+export const AchievementSelect = () => {
+  return <div>AchievementSelect</div>;
+};
---
+++ src/components/character-select.tsx
@@ -0,0 +1,3 @@
+export const CharacterSelect = () => {
+  return <div>character-select</div>;
+};
